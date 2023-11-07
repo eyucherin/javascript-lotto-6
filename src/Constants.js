d31b68d152fcea1b5d218aef3d92d2af7b7c7dc3
eyucherin <yu24c@mtholyoke.edu>
2023-11-07 14:05:22 -0500

    feat(constants) - fix constants name

--- src/Constants.js
+++ src/Constants.js
@@ -1,14 +1,14 @@
-const CONSTANTS = {
+const CONSTANT_MESSAGE = {
   MIN_LOTTO_NUMBER: 1,
   MAX_LOTTO_NUMBER: 45,
   LOTTO_NUMBER_COUNT: 6,
   PRICE_PER_TICKET: 1000,
 };
 
-const ERROR = {
+const ERROR_MESSAGE = {
     ERROR_MSG_INVALID_NUMBER_COUNT: "[ERROR] 로또 번호는 6개여야 합니다.",
     ERROR_MSG_DUPLICATE_NUMBER : "[ERROR] 로또 번호는 중복될 수 없습니다.", 
     ERROR_MSG_NUMBER_OUT_OF_RANGE : "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
 }
 
-export{CONSTANTS,ERROR}
+export{CONSTANT_MESSAGE,ERROR_MESSAGE}
