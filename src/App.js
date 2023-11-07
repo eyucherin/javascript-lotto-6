67eeaf58cfec9770853421a6bdaeb384238d77d7
eyucherin <yu24c@mtholyoke.edu>
2023-11-07 18:58:07 -0500

    feat(App) - ask user for purchase amount and validate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { ERROR_MESSAGE } from './Constants.js';
+
 class App {
-  async play() {}
+  async play() {
+    try{
+      const purchaseAmount = await this.validatePurchaseAmount();
+
+    }
+    catch(error){
+      console.error(error.message);
+    }
+  }
+
+  async validatePurchaseAmount() {
+    const purchaseAmount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+
+    const intAmount = parseInt(purchaseAmount, 10);
+
+    if (isNaN(intAmount) || intAmount <= 0 || intAmount % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGE.COST_NOT_DIVISIBLE);
+    }
+
+    return intAmount;
+  }
 }
 
 export default App;
