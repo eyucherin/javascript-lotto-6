4488624140edd17ac2529168f6df49c372a7e9d1
eyucherin <yu24c@mtholyoke.edu>
2023-11-07 15:34:44 -0500

    feat(Game) - create generateTicket method to create a list of Lotto tickets

--- src/Game.js
+++ src/Game.js
@@ -10,4 +10,13 @@ class Game {
         this.#winningNumbers = winningNum;
         this.#bonusNumber = bonusNum;
     }
+
+    generateTickets(numberOfTicket){
+        const tickets = [];
+        for(let i = 0; i < numberOfTicket; i++){
+            const numbers = Random.pickUniqueNumbersInRange(CONSTANT.MIN_LOTTO_NUMBER, CONSTANT.MAX_LOTTO_NUMBER, CONSTANT.LOTTO_NUMBER_COUNT);
+            tickets.push(new Lotto(numbers));
+        }
+        return tickets;
+    }
 }
